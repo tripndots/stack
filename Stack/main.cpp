93fe46d1bda7241dd2223f57316534a7d74b17b2
Federico Dottin <cavaleria52@gmail.com>
2017-04-24 22:36:11 -0700

    Initial Commit

---
+++ Stack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Stack
+//
+//  Created by Federico Dottin on 4/24/17.
+//  Copyright © 2017 Federico Dottin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
