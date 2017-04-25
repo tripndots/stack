7bfa6d71e3eeabba5903d8ec093c9487050f049e
Federico Dottin <cavaleria52@gmail.com>
2017-04-25 01:52:25 -0700

    inital commit

--- Stack/main.cpp
+++ Stack/main.cpp
@@ -7,9 +7,61 @@
 //
 
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+template <class T>
+class Stack{
+public:
+    Stack() {
+        head = NULL;
+    }
+    void push(T data) {
+        node* newNode = new node;
+        newNode->data = data;
+        if(head == NULL) {
+            head = newNode;
+            newNode->next = NULL;
+        } else {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+    void print() {
+        itr = head;
+        while (itr != NULL) {
+            cout << itr->data << endl;
+            itr = itr->next;
+        }
+    }
+    T pop() {
+        if(head == NULL)
+            throw "Cannot Pop when the stack is empty";
+        T ret = head->data;
+        node* toBeDeleted = head;
+        head = head->next;
+        delete toBeDeleted;
+        return ret;
+    }
+private:
+    struct node {
+        node* next;
+        T data;
+    };
+    node* head;
+    node* itr;
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Stack<int> myStack;
+    
+    myStack.push(15);
+    myStack.push(30);
+    myStack.push(378);
+    myStack.push(23);
+    cout << myStack.pop() << " has been popped" << endl;
+    myStack.print();
+    
     return 0;
 }
